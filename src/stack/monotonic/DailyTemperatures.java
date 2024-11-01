50127f8a3d7e5b004f69d5faf1373ec3aef9de19
jinvicky <jinvicky@intube.kr>
2024-11-01 18:02:25 +0900

    feat: DailyTemperatures 풀이중

--- src/stack/monotonic/DailyTemperatures.java
+++ src/stack/monotonic/DailyTemperatures.java
@@ -2,6 +2,7 @@ package stack.monotonic;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 /**
  * LeetCode 739. Daily Temperatures
@@ -11,10 +12,24 @@ public class DailyTemperatures {
     public int[] dailyTemperatures(int[] temperatures) {
         int[] ans = new int[temperatures.length];
         Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
 
+        for(int i = 0; i < temperatures.length; i++) {
 
-
-
+            if(stack.isEmpty()) {
+                // do nothing
+            } else {
+                int count = 0;
+                int insertIndex = 0;
+                while(!stack.isEmpty() && stack.peek() > temperatures[i]) {
+                    insertIndex = map.get(stack.pop());
+                    count++;
+                }
+                ans[insertIndex] = count;
+            }
+            stack.push(temperatures[i]);
+            map.put(temperatures[i], i);
+        }
         return ans;
     }
 
